f948f9ad9e4d5ca873b2a1a5f59b45bf8abde6c7
j <you@example.com>
2026-03-26 23:15:23 +0000

    remove delay for websockets

--- src/ws/impl/sys/WebSocket.hx
+++ src/ws/impl/sys/WebSocket.hx
@@ -9,223 +9,208 @@ import sys.ssl.Socket as SecureSocket;
 
 @:noCompletion
 class WebSocket extends WebSocketBase {
-    public var _protocol:String;
-    public var _host:String;
-    public var _port:Int = 0;
-    public var _path:String;
+	public var _protocol:String;
+	public var _host:String;
+	public var _port:Int = 0;
+	public var _path:String;
 
-    private var _processThread:Thread;
-    private var _encodedKey:String = "wskey";
+	private var _processThread:Thread;
+	private var _encodedKey:String = "wskey";
 
-    public var binaryType:BinaryType;
+	public var binaryType:BinaryType;
 
-    public var additionalHeaders(get, null):Map<String, String>;
+	public var additionalHeaders(get, null):Map<String, String>;
 
-    public function new(url:String, immediateOpen = true) {
-        parseUrl(url);
+	public function new(url:String, immediateOpen = true) {
+		parseUrl(url);
 
-        super(createSocket());
+		super(createSocket());
 
-        if (immediateOpen) {
-            open();
-        }
-    }
+		if (immediateOpen) {
+			open();
+		}
+	}
 
-    inline private function parseUrl(url) {
-        var urlRegExp = ~/^(\w+?):\/\/([\w\.-]+)(:(\d+))?(\/.*)?$/;
+	inline private function parseUrl(url) {
+		var urlRegExp = ~/^(\w+?):\/\/([\w\.-]+)(:(\d+))?(\/.*)?$/;
 
-        if ( ! urlRegExp.match(url)) {
-            throw 'Uri not matching websocket URL "${url}"';
-        }
+		if (!urlRegExp.match(url)) {
+			throw 'Uri not matching websocket URL "${url}"';
+		}
 
-        _protocol = urlRegExp.matched(1);
+		_protocol = urlRegExp.matched(1);
 
-        _host = urlRegExp.matched(2);
+		_host = urlRegExp.matched(2);
 
-        var parsedPort = Std.parseInt(urlRegExp.matched(4));
-        if (parsedPort > 0 ) {
-            _port = parsedPort;
-        }
-        _path = urlRegExp.matched(5);
-        if (_path == null || _path.length == 0) {
-            _path = "/";
-        }
-    }
+		var parsedPort = Std.parseInt(urlRegExp.matched(4));
+		if (parsedPort > 0) {
+			_port = parsedPort;
+		}
+		_path = urlRegExp.matched(5);
+		if (_path == null || _path.length == 0) {
+			_path = "/";
+		}
+	}
 
-    private function createSocket():Socket {
-        if (_protocol == "wss") {
-            /*
-            #if (java || cs)
-                throw "Secure sockets not implemented";
-            #else
-						*/
-                if (_port == 0) {
-                    _port = 443;
-                }
-                return new SecureSocket();
-            
-            
-            // throw "todo wss";
-        } else if (_protocol == "ws") {
-            if (_port == 0) {
-                _port = 80;
-            }
-            return new Socket();
-        } else {
-            throw 'Unknown protocol $_protocol';
-        }
-    }
+	private function createSocket():Socket {
+		if (_protocol == "wss") {
+			/*
+				#if (java || cs)
+					throw "Secure sockets not implemented";
+				#else
+			 */
+			if (_port == 0) {
+				_port = 443;
+			}
+			return new SecureSocket();
 
+			// throw "todo wss";
+		} else if (_protocol == "ws") {
+			if (_port == 0) {
+				_port = 80;
+			}
+			return new Socket();
+		} else {
+			throw 'Unknown protocol $_protocol';
+		}
+	}
 
-    public function open() {
-        if (state != State.Handshake) {
-            throw "Socket already connected";
-        }
-        _socket.setBlocking(true);
-        _socket.connect(new sys.net.Host(_host), _port);
-        _socket.setBlocking(false);
+	public function open() {
+		if (state != State.Handshake) {
+			throw "Socket already connected";
+		}
+		_socket.setBlocking(true);
+		_socket.connect(new sys.net.Host(_host), _port);
+		_socket.setBlocking(false);
 
-        #if !cs
+		#if !cs
+		#if websockets_threaded
+		_processThread = Thread.create(processThread);
+		_processThread.sendMessage(this);
+		#else
+		var event:haxe.MainLoop.MainEvent = null;
+		event = haxe.MainLoop.add(() -> {
+			if (this.state != State.Closed) { // TODO: should think about mutex
+				this.process();
+			} else {
+				event.stop();
+			}
+		});
+		#end
+		#else
+		#if websockets_threaded
+		haxe.MainLoop.addThread(function() {
+			processLoop(this);
+		});
+		#else
+		var event:haxe.MainLoop.MainEvent = null;
+		event = haxe.MainLoop.add(() -> {
+			if (this.state != State.Closed) { // TODO: should think about mutex
+				this.process();
+			} else {
+				event.stop();
+			}
+		});
+		#end
+		#end
 
-        #if websockets_threaded
+		sendHandshake();
+	}
 
-        _processThread = Thread.create(processThread);
-        _processThread.sendMessage(this);
+	private function processThread() {
+		var ws:WebSocket = Thread.readMessage(true);
+		processLoop(this);
+	}
 
-        #else
+	private function processLoop(ws:WebSocket) {
+		while (ws.state != State.Closed) {
+			ws.process();
+			// Sys.sleep(0.1);						//
+		}
+	}
 
-        var event:haxe.MainLoop.MainEvent = null;
-        event = haxe.MainLoop.add(() -> {
-            if (this.state != State.Closed) { // TODO: should think about mutex
-                this.process();
-            } else {
-                event.stop();
-            }
-        });
+	function get_additionalHeaders() {
+		if (additionalHeaders == null) {
+			additionalHeaders = new Map<String, String>();
+		}
+		return additionalHeaders;
+	}
 
-        #end
+	public function sendHandshake() {
+		var httpRequest = new HttpRequest();
+		httpRequest.method = "GET";
+		// TODO: should propably be hostname+port+path?
+		httpRequest.uri = _path;
+		httpRequest.httpVersion = "HTTP/1.1";
 
-        #else
+		httpRequest.headers.set(HttpHeader.HOST, _host + ":" + _port);
+		httpRequest.headers.set(HttpHeader.USER_AGENT, "haxeui-core/websockets");
+		httpRequest.headers.set(HttpHeader.SEC_WEBSOSCKET_VERSION, "13");
+		httpRequest.headers.set(HttpHeader.UPGRADE, "websocket");
+		httpRequest.headers.set(HttpHeader.CONNECTION, "Upgrade");
+		httpRequest.headers.set(HttpHeader.PRAGMA, "no-cache");
+		httpRequest.headers.set(HttpHeader.CACHE_CONTROL, "no-cache");
+		httpRequest.headers.set(HttpHeader.ORIGIN, _socket.host().host.toString() + ":" + _socket.host().port);
 
-        #if websockets_threaded
+		_encodedKey = generateWSKey();
+		httpRequest.headers.set(HttpHeader.SEC_WEBSOCKET_KEY, _encodedKey);
 
-        haxe.MainLoop.addThread(function() {
-            processLoop(this);
-        });
+		if (additionalHeaders != null) {
+			for (k in additionalHeaders.keys()) {
+				httpRequest.headers.set(k, additionalHeaders[k]);
+			}
+		}
 
-        #else
+		sendHttpRequest(httpRequest);
+	}
 
-        var event:haxe.MainLoop.MainEvent = null;
-        event = haxe.MainLoop.add(() -> {
-            if (this.state != State.Closed) { // TODO: should think about mutex
-                this.process();
-            } else {
-                event.stop();
-            }
-        });
+	private override function handleData() {
+		switch (state) {
+			case State.Handshake:
+				var httpResponse = recvHttpResponse();
+				if (httpResponse == null) {
+					return;
+				}
 
-        #end
+				handshake(httpResponse);
+				handleData();
+			case _:
+				super.handleData();
+		}
+	}
 
-        #end
+	private function handshake(httpResponse:HttpResponse) {
+		if (httpResponse.code != 101) {
+			if (onerror != null) {
+				onerror(httpResponse.headers.get(HttpHeader.X_WEBSOCKET_REJECT_REASON));
+			}
+			close();
+			return;
+		}
 
-        sendHandshake();
-    }
+		var secKey = httpResponse.headers.get(HttpHeader.SEC_WEBSOSCKET_ACCEPT);
 
-    private function processThread() {
-        var ws:WebSocket = Thread.readMessage(true);
-        processLoop(this);
-    }
+		if (secKey == null) {
+			trace("This server does not implement Sec-WebSocket-Key.");
+		} else {
+			if (secKey != makeWSKeyResponse(_encodedKey)) {
+				if (onerror != null) {
+					onerror("Error during WebSocket handshake: Incorrect 'Sec-WebSocket-Accept' header value");
+				}
+				close();
+				return;
+			}
+		}
 
-    private function processLoop(ws:WebSocket) {
-        while (ws.state != State.Closed) {
-            ws.process();
-            Sys.sleep(0.1);
-        }
-    }
+		_onopenCalled = false;
+		state = State.Head;
+	}
 
-    function get_additionalHeaders() {
-        if (additionalHeaders == null) {
-            additionalHeaders = new Map<String, String>();
-        }
-        return additionalHeaders;
-    }
-
-    public function sendHandshake() {
-        var httpRequest = new HttpRequest();
-        httpRequest.method = "GET";
-        // TODO: should propably be hostname+port+path?
-        httpRequest.uri = _path;
-        httpRequest.httpVersion = "HTTP/1.1";
-
-        httpRequest.headers.set(HttpHeader.HOST, _host + ":" + _port);
-        httpRequest.headers.set(HttpHeader.USER_AGENT, "haxeui-core/websockets");
-        httpRequest.headers.set(HttpHeader.SEC_WEBSOSCKET_VERSION, "13");
-        httpRequest.headers.set(HttpHeader.UPGRADE, "websocket");
-        httpRequest.headers.set(HttpHeader.CONNECTION, "Upgrade");
-        httpRequest.headers.set(HttpHeader.PRAGMA, "no-cache");
-        httpRequest.headers.set(HttpHeader.CACHE_CONTROL, "no-cache");
-        httpRequest.headers.set(HttpHeader.ORIGIN, _socket.host().host.toString() + ":" + _socket.host().port);
-
-        _encodedKey = generateWSKey();
-        httpRequest.headers.set(HttpHeader.SEC_WEBSOCKET_KEY, _encodedKey);
-
-        if (additionalHeaders != null) {
-            for ( k in additionalHeaders.keys()) {
-                httpRequest.headers.set(k, additionalHeaders[k]);
-            }
-        }
-
-        sendHttpRequest(httpRequest);
-    }
-
-    private override function handleData() {
-        switch (state) {
-            case State.Handshake:
-                var httpResponse = recvHttpResponse();
-                if (httpResponse == null) {
-                    return;
-                }
-
-                handshake(httpResponse);
-                handleData();
-            case _:
-                super.handleData();
-        }
-
-    }
-
-    private function handshake(httpResponse:HttpResponse) {
-        if (httpResponse.code != 101) {
-            if (onerror != null) {
-                onerror(httpResponse.headers.get(HttpHeader.X_WEBSOCKET_REJECT_REASON));
-            }
-            close();
-            return;
-        }
-
-        var secKey = httpResponse.headers.get(HttpHeader.SEC_WEBSOSCKET_ACCEPT);
-        
-        if(secKey == null) {
-            trace("This server does not implement Sec-WebSocket-Key.");
-        } else {
-            if (secKey != makeWSKeyResponse(_encodedKey)) {
-                if (onerror != null) {
-                    onerror("Error during WebSocket handshake: Incorrect 'Sec-WebSocket-Accept' header value");
-                }
-                close();
-                return;
-            }
-        }
-
-        _onopenCalled = false;
-        state = State.Head;
-    }
-
-    private function generateWSKey():String {
-        var b = Bytes.alloc(16);
-        for (i in 0...16) {
-            b.set(i, Std.random(255));
-        }
-        return Base64.encode(b);
-    }
+	private function generateWSKey():String {
+		var b = Bytes.alloc(16);
+		for (i in 0...16) {
+			b.set(i, Std.random(255));
+		}
+		return Base64.encode(b);
+	}
 }
